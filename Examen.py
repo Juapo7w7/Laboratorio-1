20eee33f0cf48a85bd6c0e9f90d9eb4fbfd7d72d
Oliver <olivermolina697@gmail.com>
2023-03-29 08:41:49 -0600

    se a relizado la primera parte

---
+++ Examen.py
@@ -0,0 +1,4 @@
+'''Pedirle dos numeros al usuario y luego con esos dos numeros hacer una suma, una resta, una multiplicacion y una division'''
+no1=int(input ("Ingrese un numero ")) 
+no2=int(input ("Ingrese otro numero "))
+suma1=no2+no1
